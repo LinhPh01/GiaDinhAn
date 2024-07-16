32bd62a869f2e60cd7edb998e443849b5abaad33
LinhPh01 <phamhoailinh779@gmail.com>
2024-07-16 15:29:36 +0700

    new pagess

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,11 @@ export default () => {
     base: "/",
     plugins: [react()],
     build: {
+      outDir: 'build',
       polyfillModulePreload:false,
       rollupOptions: {
       output:{
+        
       manualChunks(id) {
       if (id.includes('node_modules')) {
       return id.toString().split('node_modules/')[1].split('./')[0].toString();
