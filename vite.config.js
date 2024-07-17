3057f5432713676d6ce93b777c84f18b55577a90
LinhPh01 <phamhoailinh779@gmail.com>
2024-07-17 10:03:51 +0700

    new pagesss

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,20 @@ export default () => {
    
     base: "",
     plugins: [react()],
-    // build: {
-    //   outDir: 'build',
-    //   polyfillModulePreload:false,
-    //   rollupOptions: {
-    //   output:{
+    build: {
+      outDir: 'dist',
+      polyfillModulePreload:false,
+      rollupOptions: {
+      output:{
         
-    //   manualChunks(id) {
-    //   if (id.includes('node_modules')) {
-    //   return id.toString().split('node_modules/')[1].split('./')[0].toString();
-    //   }
-    //   }
-    //   }
-    //   }
-    //   }
+      manualChunks(id) {
+      if (id.includes('node_modules')) {
+      return id.toString().split('node_modules/')[1].split('./')[0].toString();
+      }
+      }
+      }
+      }
+      }
       });
 
       
